d1762918e755668864d6ee90985df2cea8457f75
Zinedine <megnouche.z@gmail.com>
2022-04-01 11:47:02 +0200

    Ajout test getAll

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -1,6 +1,8 @@
 
-//const calc = require('Add')
+
 import { Add, Substract, Multiply } from './calc.mjs';
+import { getAll } from "./../server/products.mjs";
+import products from "./../server/products.json"
 
 //Test de la fonction Add()
 test('Additione 2 nombre: (1+2)', () => {
@@ -16,3 +18,7 @@ test('Soustrait 2 nombre: (10-4)', () => {
 test('Multiplie 2 nombre: (4x5)', () => {
     expect(Multiply(4, 5)).toBe(20);
 });
+// Mauvaise maniere
+/*test('Fonction getAll()', () => {
+    expect(getAll()).toMatchObject(products);
+});*/
